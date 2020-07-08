8f04478313ad08b425422a8e9a507475af4fcca5
suhas.r@codemonk.in <suhas.r@codemonk.in>
2020-07-08 23:33:25 +0530

    update: settings.py

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='/path/to/static/files')
+application.add_files('/path/to/more/static/files', prefix='more-files/')
